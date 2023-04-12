d61256d76baab24c9612f97c6e8b4ad2f712ab92
arunpradhanang <127901750+arunpradhanang@users.noreply.github.com>
2023-04-12 16:24:36 -0400

    adjusting transition

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 const swup = new Swup();
-// run once when page loads
-if (document.readyState === 'complete') {
-    init();
-  } else {
-    document.addEventListener('DOMContentLoaded', () => init());
-  }
+
   
-  // run after every additional navigation by swup
-  swup.on('contentReplaced', init);
-  swup.on('willReplaceContent', unload);
+ 
